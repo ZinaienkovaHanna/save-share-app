b4814e4e52ba44e3363a730bae370b312bacdead
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2024-01-16 12:11:59 +0200

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import App from './containers/App';
+
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
